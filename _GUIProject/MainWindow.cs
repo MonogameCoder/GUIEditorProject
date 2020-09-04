d8d6dbad2837db408cb61a41c32eb88794c2da0e
ForbiddenArea <romuloromerodeveloper@gmail.com>
2020-09-04 13:49:07 -0300

    Restricted to the item's drag position to the Frame's Bounds

--- _GUIProject/MainWindow.cs
+++ _GUIProject/MainWindow.cs
@@ -25,13 +25,13 @@ namespace _GUIProject
     // Implement export and save, by exporting all to XML using Serialization
     public class MainWindow : Game
     {
-        public static MainWindow MainInstance;    
+        public static MainWindow MainInstance;
 
         // Basic frame elements       
-        public static IContainer RootContainer { get; set; }         
+        public static IContainer RootContainer { get; set; }
         public static UIObject CurrentObject { get; set; }
 
-        public static SpriteBatch _mainBatch;      
+        public static SpriteBatch _mainBatch;
         public static ElementSelection Selection { get; private set; }
         private SpriteBatch _spriteBatch;
 
@@ -64,17 +64,19 @@ namespace _GUIProject
         {
             _guiList.Remove(RootContainer);
         }
-   
+
         public MainWindow()
         {
 
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
+
+
             Window.AllowUserResizing = true;
             IsMouseVisible = true;
-         
-        }     
+
+        }
         public void SetMouseVisibility(bool isVisible)
         {
             IsMouseVisible = isVisible;
@@ -83,8 +85,9 @@ namespace _GUIProject
 
         protected override void Initialize()
         {
+
             _guiList = new List<IObject>();
-        
+
             MainInstance = this;
 
             // This is necessary for performance, it caches the resources before first use
@@ -93,11 +96,11 @@ namespace _GUIProject
             caching.InitPropertyPanel();
 
             MouseGUI.Load();
-            
+
             _fileMenu = new ComboBox();
             _fileMenu.Position = new Point(32, 32);
-            _fileMenu.Initialize();          
-            _fileMenu.Editable = false;           
+            _fileMenu.Initialize();
+            _fileMenu.Editable = false;
 
             _fileMenu.AddName("File", Color.Black, Singleton.Font.GetFont(FontManager.FontType.LUCIDA_CONSOLE));
 
@@ -117,7 +120,7 @@ namespace _GUIProject
 
             _fileMenu.AddNewItem("Export", () =>
             {
-               // TODO: 
+                // TODO: 
 
                 _fileMenu.Hide();
             });
@@ -125,25 +128,25 @@ namespace _GUIProject
             {
                 Exit();
             });
-       
-          
+
+
             // Toolbox elements
             UIToolShelf = new Frame("UItoolboxTX", DrawPriority.NORMAL, MoveOption.STATIC);
             UIToolShelf.Position = new Point(32, 100);
             UIToolShelf.Initialize();
-            
+
 
             _toolShelfComboBox = new ComboBox();
             _toolShelfComboBox.Initialize();
-            _toolShelfComboBox.Editable = true;;
+            _toolShelfComboBox.Editable = true; ;
             _toolShelfComboBox.DefaultOffset = new Point(8, 43);
-        
+
             _toolShelfCheckbox = new CheckBox();
             _toolShelfCheckbox.Initialize();
             _toolShelfCheckbox.Text = "CheckBox";
             _toolShelfCheckbox.Editable = true; ;
 
-            _toolShelfLabel = new Label();            
+            _toolShelfLabel = new Label();
             _toolShelfLabel.Initialize();
             _toolShelfLabel.TextColor.Color = Color.White;
             _toolShelfLabel.Editable = true; ;
@@ -171,7 +174,7 @@ namespace _GUIProject
             _toolShelfMultilineTextbox.Initialize();
             _toolShelfMultilineTextbox.Editable = true; ;
 
-            UIToolShelf.AddItem(new Point(32, 32), _toolShelfTextbox);    
+            UIToolShelf.AddItem(new Point(32, 32), _toolShelfTextbox);
             UIToolShelf.AddItem(new Point(32, 96), _toolShelfLabel);
             UIToolShelf.AddItem(new Point(32, 160), _toolShelfButton);
             UIToolShelf.AddItem(new Point(32, 224), _toolShelfComboBox);
@@ -179,53 +182,53 @@ namespace _GUIProject
             UIToolShelf.AddItem(new Point(32, 352), _toolShelfToggle);
             UIToolShelf.AddItem(new Point(16, 416), _toolShelfSlider);
             UIToolShelf.AddItem(new Point(16, 480), _toolShelfMultilineTextbox);
-          
+
 
             Selection = new ElementSelection();
             Selection.Initialize();
             Selection.Editable = false;
             Selection.Position = Point.Zero;
 
-            UIToolShelf.Show();     
+            UIToolShelf.Show();
 
             _mainFrameCaption = new Label("This is the Actual Window");
             _mainFrameCaption.Initialize();
             _mainFrameCaption.TextFont = Singleton.Font.GetFont(FontManager.FontType.GEORGIA);
-          
-            
+
+
             // Main frame layout test
-            RootContainer = new Grid();           
+            RootContainer = new Grid();
             RootContainer.Initialize();
             RootContainer.Position = new Point(568, 100);
 
-            RootContainer.Show();   
-            
+            RootContainer.Show();
+
 
             _guiList.Add(Selection);
             _guiList.Add(_fileMenu);
-            _guiList.Add(UIToolShelf);     
+            _guiList.Add(UIToolShelf);
             _guiList.Add(RootContainer);
-     
+
 
             base.Initialize();
         }
-       
+
 
         protected override void LoadContent()
-        {            
-            _spriteBatch = new SpriteBatch(GraphicsDevice);                  
-      
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
             MouseGUI.Setup();
-           
+
             for (int i = 0; i < _guiList.Count; i++)
-            {                
+            {
                 _guiList[i].Setup();
-            }         
+            }
 
             Vector2 textSize = _mainFrameCaption.TextFont.Font.MeasureString(_mainFrameCaption.Text);
             Vector2 textPosition = new Vector2(RootContainer.Rect.Center.X - textSize.X / 2, RootContainer.Rect.Top - textSize.Y);
             _mainFrameCaption.AddPosition(textPosition);
-            
+
             UIToolShelf.Size += new Point(0, 256);
 
             MouseGUI.AddSpriteRenderer(_spriteBatch);
@@ -248,7 +251,7 @@ namespace _GUIProject
 
         IContainer _fromContainer;
         protected override void Update(GameTime gameTime)
-        {  
+        {
             if (Singleton.Input.KeyReleased(Keys.Escape))
             {
                 Exit();
@@ -261,13 +264,13 @@ namespace _GUIProject
                 RootContainer.UpdateLayout();
             }
             MouseGUI.Update();
-           
 
-            
-            for (int i = _guiList.Count -1; i >= 0 ; i--)
+
+
+            for (int i = _guiList.Count - 1; i >= 0; i--)
             {
                 _guiList[i].Update(gameTime);
-                
+
             }
             _fileMenu.Update(gameTime);
 
@@ -280,42 +283,39 @@ namespace _GUIProject
                 MouseGUI.HitObject = _guiList[i].HitTest(MouseGUI.Position);
 
                 if (MouseGUI.HitObject != null)
-                {                   
+                {
                     break;
                 }
             }
 
             if (MouseGUI.HitObject != null)
             {
-               
+
                 if (MouseGUI.LeftWasPressed)
-                {                   
-                    MouseGUI.Focus = MouseGUI.HitObject;                
+                {
+                    MouseGUI.Focus = MouseGUI.HitObject;
                     MouseGUI.DragOffset = MouseGUI.Focus.Position - MouseGUI.Position;
-                    
-                    _fromContainer = RootContainer.Contains(MouseGUI.Focus) ?  RootContainer : UIToolShelf;
+
+                    _fromContainer = RootContainer.Contains(MouseGUI.Focus) ? RootContainer : UIToolShelf;
 
 
                     if (MouseGUI.Focus.Editable && RootContainer.Contains(MouseGUI.Focus))
-                    {                        
-                        RootContainer.RemoveItem(MouseGUI.Focus);   
+                    {
+                        RootContainer.RemoveItem(MouseGUI.Focus);
                     }
-                    
                 }
                 else
                 {
                     if (MouseGUI.Focus != null)
                     {
-                        
-                      
                         if (MouseGUI.Focus.Editable && MouseGUI.Focus.MoveState == MoveOption.DYNAMIC)
                         {
-                            // TODO: Make sure this is inside the MainWindowFrame bounds
-                        
+
                             MouseGUI.Focus.Position = MouseGUI.Position + MouseGUI.DragOffset;
                             MouseGUI.Focus.Update(gameTime);
 
-                            if (MouseGUI.LeftIsPressed && RootContainer.Contains(MouseGUI.Focus.Position))
+                            // TODO: Make sure this is inside the MainWindowFrame bounds
+                            if (MouseGUI.LeftIsPressed && RootContainer.Contains(MouseGUI.Position))
                             {
                                 Point newItemPosition = RootContainer.SimulateInsert(MouseGUI.Focus.Position - RootContainer.Position, MouseGUI.Focus);
                                 newItemPosition += RootContainer.Position;
@@ -335,7 +335,7 @@ namespace _GUIProject
 
                         if (MouseGUI.LeftWasReleased)
                         {
-                           
+
                             Selection.Hide();
                             if (MouseGUI.Focus.Editable && RootContainer.Contains(MouseGUI.Focus) && _fromContainer != UIToolShelf)
                             {
@@ -349,7 +349,7 @@ namespace _GUIProject
 
                             if (MouseGUI.Focus is UIObject && !MouseGUI.Focus.Editable)
                             {
-                               
+
                                 MouseGUI.Focus.MouseEvent.Click();
                             }
 
@@ -357,10 +357,10 @@ namespace _GUIProject
                             if (MouseGUI.Focus.Editable && RootContainer.Contains(MouseGUI.Focus.Position) && _fromContainer == UIToolShelf)
                             {
                                 CurrentObject = UIFactory<UIObject, IContainer>.CreateObject(MouseGUI.Focus.Position, MouseGUI.Focus, RootContainer);
-                                CurrentObject.InitPropertyPanel();                           
+                                CurrentObject.InitPropertyPanel();
                                 CurrentObject.AddSpriteRenderer(_mainBatch);
                                 CurrentObject.AddStringRenderer(_mainBatch);
-                                CurrentObject.AddPropertyRenderer(_mainBatch);                              
+                                CurrentObject.AddPropertyRenderer(_mainBatch);
                                 RootContainer.Update(gameTime);
                                 RootContainer.UpdateLayout();
 
@@ -369,31 +369,61 @@ namespace _GUIProject
                             {
                                 CurrentObject = MouseGUI.Focus;
                             }
-                           
+
                             MouseGUI.ClearFocus();
                         }
-
                     }
                 }
-              
-            }    
+
+            }
+            if (MouseGUI.Focus != null)
+            {
+                if (MouseGUI.LeftIsPressed && _fromContainer != UIToolShelf)
+                {
+
+                    if (MouseGUI.Position.X <= RootContainer.Rect.Left)
+                    {
+                        MouseGUI.Focus.Position =
+                            new Point(RootContainer.Rect.Left, MouseGUI.Position.Y);
+                    }
+                    if (MouseGUI.Position.X + MouseGUI.Focus.Width >= RootContainer.Rect.Right)
+                    {
+                        MouseGUI.Focus.Position =
+                            new Point(RootContainer.Rect.Right - MouseGUI.Focus.Width, MouseGUI.Position.Y);
+                    }
+                    if (MouseGUI.Position.Y + MouseGUI.Focus.Height >= RootContainer.Rect.Bottom)
+                    {
+                        MouseGUI.Focus.Position =
+                           new Point(MouseGUI.Position.X, RootContainer.Rect.Bottom - MouseGUI.Focus.Height);
+                    }
+                    if (MouseGUI.Position.Y - MouseGUI.Focus.Height <= RootContainer.Rect.Top)
+                    {
+                        MouseGUI.Focus.Position =
+                          new Point(MouseGUI.Position.X, RootContainer.Rect.Top);
+
+                    }
+
+                    MouseGUI.Focus.Update(gameTime);
+                }
+            }
+
             base.Update(gameTime);
         }
 
-      
+
         protected override void Draw(GameTime gameTime)
-        {             
+        {
 
             GraphicsDevice.Clear(Color.Gray);
 
             _spriteBatch.Begin(SpriteSortMode.Deferred);
 
-            for (int i = _guiList.Count -1; i >= 0; i--)
+            for (int i = _guiList.Count - 1; i >= 0; i--)
             {
                 _guiList[i].Draw();
             }
             MouseGUI.Draw();
-       
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
